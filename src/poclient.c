874c81fca5a17e1f2b7d6aae0c008dccf59a860a
Claudiu Ghioc <claudiu.ghioc@gmail.com>
2012-10-30 01:39:34 +0200

    Remove binary files from git versioning
    Signed-off-by: Claudiu Ghioc <claudiu.ghioc@gmail.com>

---
+++ src/poclient.c
@@ -0,0 +1,47 @@
+#include <stdio.h> 
+#include <time.h> 
+#include <rpc/rpc.h> 
+
+#include "load.h" 
+#define RMACHINE "localhost"
+
+int main(int argc, char *argv[]){
+    int lat, lon;
+    char *file;
+
+    if (argc != 4) {
+        printf("Use lat long filename as command line arguments\n");
+        return 1;
+    }
+    lat = atoi(argv[1]);
+    lon = atoi(argv[2]);
+    file = argv[3];
+
+	/* variabila clientului */
+	CLIENT *handle;
+
+	handle=clnt_create(
+		RMACHINE,		/* numele masinii unde se afla server-ul */
+		LOAD_PROG,		/* numele programului disponibil pe server */
+		LOAD_VERS,		/* versiunea programului */
+		"tcp");			/* tipul conexiunii client-server */
+	
+	if(handle == NULL) {
+		perror("");
+		return -1;
+	}
+
+    printf("Ies din client\n");
+
+    /* call to RPC server
+    struct data date;
+    struct data* res;
+    date.text = calloc(1000, 1);
+    printf("Introduceti commanda:\n");
+    gets(date.text);
+    res = get_load_1(&date, handle); 
+    printf("%s\n", res->text);
+    */
+	
+	return 0;
+}
